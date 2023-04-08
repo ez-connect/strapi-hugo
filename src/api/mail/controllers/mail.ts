253d3fe52aa473d8323df698ebe662033630514b
Vinh <thanh.vinh@hotmail.com>
2023-04-08 23:03:27 +0700

    Fixed email endpoint

--- src/api/mail/controllers/mail.ts
+++ src/api/mail/controllers/mail.ts
@@ -24,10 +24,9 @@ export default {
     }
 
     // Email data
-    let slug = `${require('slug')(entry.title || entry.name)}-${entry.id}`;
-    if (entry.parent) {
-      slug = `${entry.parent}/${slug}`;
-    }
+    const { title, path } = entry.title;
+    const slug = `${require('slug')(entry.title || entry.name)}-${entry.id}`;
+    const url = path ? `${path}/${slug}` : `${model}/${slug}`;
 
     const subscriptions = await strapi
       .service('api::mail.mail')
@@ -35,21 +34,24 @@ export default {
     subscriptions.map((e) => {
       console.log('Send email to: ', e.email);
       // https://docs.strapi.io/developer-docs/latest/plugins/email.html#programmatic-usage
-      // strapi.plugins['email-designer'].services.email.sendTemplatedEmail(
-      //   {
-      //     to: e.email,
-      //     // from: is not specified, so it's the defaultFrom that will be used instead
-      //   },
-      //   {
-      //     templateReferenceId: template.templateId,
-      //     subject: _.template(template.subject, { entry }).data,
-      //   },
-      //   {
-      //     model,
-      //     slug,
-      //     entry,
-      //   },
-      // );
+      strapi
+        .plugin('email-designer')
+        .service('email')
+        .sendTemplatedEmail(
+          {
+            to: e.email,
+            // from: is not specified, so it's the defaultFrom that will be used instead
+          },
+          {
+            templateReferenceId: template.templateId,
+            subject: _.template(template.subject)({ title }),
+          },
+          {
+            model,
+            url,
+            entry,
+          },
+        );
     });
 
     ctx.body = subscriptions;
